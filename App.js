c8fd0dc582614dc20eac78d7f9433f2939497b52
FILLALI Nada <adalinadalin96@gmail.com>
2023-05-15 10:18:57 -0700

    added src folder with uncoming weather and current weather improved style for the UpcomingWeather component

--- App.js
+++ App.js
@@ -1,58 +1,19 @@
 import React from "react";
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import CurrentWeather from "./src/components/CurrentWeather";
+import UpComingWeather from "./src/components/UpcomingWeather";
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.wrapper}>
-      <View style={styles.container}>
-        <Text style={styles.temp}>6</Text>
-        <Text style={styles.feels}>Feels like 5</Text>
-        <View styles={styles.highlowWrapper}>
-          <Text style={styles.highlow}>High: 8</Text>
-          <Text style={styles.highlow}>Low: 6</Text>
-        </View>
-      </View>
-      <View style={styles.bodyWrapper}>
-        <Text style={styles.description}>It's sunny</Text>
-        <Text style={styles.message}>It's perfect t-shirt weather</Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <UpComingWeather />
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center'
-  },
-  wrapper: {
-    flex: 1,
-    backgroundColor: 'pink',
-  },
-  temp: {
-    color: 'black',
-    fontSize: 48
-  },
-  feels: {
-    fontSize: 30,
-    color: 'black'
-  },
-  highlow: {
-    color: 'black',
-    fontSize: 20
-  },
-  highlowWrapper: {
-    flexDirection: 'row'
-  },
-  bodyWrapper: {
-    justifyContent: 'flex-end',
-    alignItems: 'flex-start'
-  },
-  description: {
-    fontSize: 48
-  },
-  message: {
-    fontSize: 30
+    flex: 1
   }
 })
 export default App
