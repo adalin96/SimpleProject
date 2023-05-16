4884500b3a63de202ea00b4d03c87bd5f63f3b33
FILLALI Nada <adalinadalin96@gmail.com>
2023-05-16 15:17:31 -0700

    added some style to the bottom navigation tab using icons

--- App.js
+++ App.js
@@ -4,16 +4,58 @@ import UpComingWeather from './src/screens/UpcomingWeather'
 import City from './src/screens/City'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Feather } from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator()
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name={'Current'} component={CurrentWeather}/>
-        <Tab.Screen name={'UpComing'} component={UpComingWeather}/>
-        <Tab.Screen name={'City'} component={City}/>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'grey'
+        }}
+      >
+        <Tab.Screen
+          name={'Current'}
+          component={CurrentWeather}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name={'droplet'}
+                size={25}
+                color={focused ? 'tomato' : 'black'}
+              />
+            )
+          }}
+        />
+        <Tab.Screen
+          name={'UpComing'}
+          component={UpComingWeather}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name={'clock'}
+                size={25}
+                color={focused ? 'tomato' : 'black'}
+              />
+            )
+          }}
+        />
+        <Tab.Screen
+          name={'City'}
+          component={City}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name={'home'}
+                size={25}
+                color={focused ? 'tomato' : 'black'}
+              />
+            )
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   )
