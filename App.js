41d71adf4ab7c7168c62d9ff9a51f31293876d88
FILLALI Nada <adalinadalin96@gmail.com>
2023-05-17 06:25:33 -0700

    added location created a .env file that seems to not be working for now

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { ActivityIndicator } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
+import { View, StyleSheet } from 'react-native'
+import * as Location from 'expo-location'
+import { TEST_KEY } from '@env'
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+console.log(TEST_KEY)
 
 const App = () => {
+  const [loading, setLoading] = useState(true)
+  const [location, setLocation] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    ;(async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        setError('permission to access location was denied')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+    })()
+  }, [])
+
+  if (location) {
+    console.log(location)
+  }
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'green'} />
+      </View>
+    )
+  }
   return (
     <NavigationContainer>
       <Tabs />
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1
+  }
+})
 
 export default App
