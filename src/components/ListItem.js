8434b64bc7d79e07fbc08bee223850a01b905587
FILLALI Nada <adalinadalin96@gmail.com>
2023-05-31 07:11:40 -0700

    ejecting from expo and failing

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,18 +3,24 @@ import { View, Text, StyleSheet } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import { weatherType } from '../utilities/weatherType'
 import { format } from 'date-fns'
+import { te } from 'date-fns/locale'
 
 const ListItem = (props) => {
   const { dt_txt, min, max, condition } = props
   const { item, date, temp, dateTextWrapper } = styles
   return (
     <View style={item}>
-      <Feather name={weatherType[condition].icon} size={50} color={'#f0da82'} />
+      {/* <Feather name={weatherType[condition].icon} size={50} color={'#f0da82'} />
       <View style={dateTextWrapper}>
         <Text style={date}>{format(dt_txt, 'dddd')}</Text>
         <Text style={date}>{format(dt_txt, 'h:mm:ss a')}</Text>
       </View>
       <Text style={temp}>{`${Math.round(min)}° / ${Math.round(max)}°`}</Text>
+       */}
+      <Feather name={'sun'} size={50} color={'white'} />
+      <Text style={date}>{dt_txt}</Text>
+      <Text style={temp}>{min}</Text>
+      <Text style={temp}>{max}</Text>
     </View>
   )
 }
@@ -38,9 +44,9 @@ const styles = StyleSheet.create({
   date: {
     color: '#fcf7d8',
     fontSize: 15
-  },
-  dateTextWrapper: {
-    flexDirection: 'column'
   }
+  // dateTextWrapper: {
+  //   flexDirection: 'column'
+  // }
 })
 export default ListItem
--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -7,10 +7,45 @@ import {
   ImageBackground
 } from 'react-native'
 import ListItem from '../components/ListItem'
+import { te } from 'date-fns/locale'
+const DATA = [
+{   
+    dt_txt: "2021-02-19 12:00:00",
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55
+    },
+  weather: [
+    {
+      main: 'Clear'
+    }
+  ]},
+  {
+    dt_txt: "2021-02-19 15:00:00",
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55
+    },
+    weather: [
+      {
+        main: 'Clear'
+      }
+    ]
+  },
+  {
+    dt_txt: "2021-02-19 18:00:00",
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55
+    },
+    weather: [
+      {
+        main: 'Rain'
+      }
+    ]
+  }
+]
 const UpComingWeather = ({ WeatherData }) => {
-  console.log('WeatherData:', WeatherData);
-
-  
   const renderItem = ({ item }) => (
     <ListItem
       condition={item.weather[0].main}
@@ -27,7 +62,7 @@ const UpComingWeather = ({ WeatherData }) => {
         style={image}
       >
         <FlatList
-          data={WeatherData}
+          data={/*WeatherData*/DATA}
           renderItem={renderItem}
           keyExtractor={(item) => item.dt_txt}
         />
